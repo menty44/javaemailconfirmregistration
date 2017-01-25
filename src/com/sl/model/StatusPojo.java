d879066a4afeeb9eaa476e1791bbcecb344e8c04
fred <fred.oluoch@impalapay.com>
2017-01-25 09:45:35 +0300

    first commit

---
+++ src/com/sl/model/StatusPojo.java
@@ -0,0 +1,25 @@
+package com.sl.model;
+
+/*Author Oluoch */
+
+public class StatusPojo {
+    private int code;
+    private String message;
+    
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public String toString() {
+		return "StatusPojo [code=" + code + ", message=" + message + "]";
+	}
+}
---
+++ src/com/sl/util/Setup.java
@@ -0,0 +1,13 @@
+package com.sl.util;
+
+/*Author Oluoch */
+
+public class Setup {
+	  public static final String DB_URL = "jdbc:mysql://localhost:3306/demos";
+	  public static final String DB_USERNAME = "ninja";
+	  public static final String DB_PASSWORD = "ninja";
+	  public static final String MAIL_USERNAME = "menty44@gmail.com"; // like example@outlook.com
+	  public static final String MAIL_PASSWORD = "XZB9O6G8fred.";  // your mail password here
+	  public static final String MAIL_SMTP_HOST = " smtp.gmail.com"; // smtp.live.com   993 
+	  public static final String MAIL_REGISTRATION_SITE_LINK = "http://localhost:8080/demos/VerifyRegisteredEmailHash";
+}
